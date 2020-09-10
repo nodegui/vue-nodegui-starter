20d23d28eb98e17c2debcaff1a37a4c6c19ffca2
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-09-10 14:21:35 +0530

    adding starter repo

---
+++ main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue-nodegui';
+import App from './App.vue';
+
+createApp(App).mount();
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const { compilerOptions } = require('vue-nodegui/dist/vueLoader');
+
+module.exports = (env = {}) => ({
+  mode: env.prod ? 'production' : 'development',
+  devtool: 'inline-source-map',
+  entry: path.resolve(__dirname, './main.js'),
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.vue', '.json'],
+    alias: {
+      vue: path.resolve(__dirname, './node_modules/vue-nodegui/dist'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: {
+          loader: 'vue-loader',
+          options: {
+            compilerOptions,
+          },
+        },
+      },
+      {
+        test: /\.node$/,
+        use: [{ loader: 'node-loader' }, { loader: 'file-loader' }],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|bmp)$/i,
+        use: [{ loader: 'file-loader' }],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
+});
