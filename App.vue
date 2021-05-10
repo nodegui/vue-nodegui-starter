6491dc887ebb6f91034abdd58cf840384bf4395d
eLoy <zybjared@gmail.com>
2021-05-10 10:07:22 +0800

    re-add App.vue

---
+++ App.vue
@@ -0,0 +1,36 @@
+<template>
+  <vn-view>
+    <hero />
+    <vn-view id="counter" :styleSheet="counterStyle">
+      <vn-text>Counter: {{count}}</vn-text>
+      <vn-button @clicked="inc">Inc</vn-button>
+    </vn-view>
+  </vn-view>
+</template>
+
+<script>
+import Hero from './Hero.vue';
+import { ref } from '@nodegui/vue-nodegui';
+
+export default {
+  components: { Hero },
+  setup() {
+    const count = ref(0);
+    const inc = () => {
+      count.value++;
+    }
+
+    return {
+      inc,
+      count,
+      counterStyle: `
+        #counter {
+          flex-direction: row;
+          align-items: 'center';
+          justify-content: 'center';
+        }
+      `
+    }
+  }
+}
+</script>
